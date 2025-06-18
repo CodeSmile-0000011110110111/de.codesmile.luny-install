9cc9480660c1af6716d94c8dd235474f3b479fc7
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-06-18 11:53:16 +0200

    just a code formatting thing

--- Editor/LunyPackageInfo.cs
+++ Editor/LunyPackageInfo.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (C) 2021-2025 Steffen Itterheim
 // Refer to included LICENSE file for terms and conditions.
 
+using System;
+using UnityEditor;
+using UnityEngine;
+
 namespace CodeSmileEditor.Luny.Install
 {
 	public struct LunyPackageInfo
 	{
-		public string Name;
-		public string GitURL;
+		public String Name;
+		public String GitURL;
 	}
 }
